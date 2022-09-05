b1262214d53170b07ec5c8b762745b387b622f27
newwin01 <newwin0198@handong.ac.kr>
2022-09-06 07:20:48 +0900

    Create load method

--- src/com/jsc/dictionary/WordCRUD.java
+++ src/com/jsc/dictionary/WordCRUD.java
@@ -1,11 +1,16 @@
 package com.jsc.dictionary;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordCRUD implements ICRUD{
 	ArrayList<Word> list;
 	Scanner s;
+	final String fname = "dic.txt";
 
 
 	WordCRUD(Scanner s) {
@@ -107,5 +112,30 @@ public class WordCRUD implements ICRUD{
 
 	public void saveFile() {
 
+	}
+
+	public void loadFile(){
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fname));
+			String line;
+			int count=0;
+			while(true) {
+				line = br.readLine();
+				if(line==null) break;
+				String data[] = line.split("\\|");
+				int level = Integer.parseInt(data[0]);
+				String word = data[1];
+				String meaning = data[2];
+				list.add(new Word(count,level,word,meaning));
+				count++;
+			}
+			br.close();
+			System.out.println("==>"+count+" 개 로딩 완료!");
+
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 존재하지 않습니다!");
+		} catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 }
--- src/com/jsc/dictionary/WordManager.java
+++ src/com/jsc/dictionary/WordManager.java
@@ -12,6 +12,7 @@ public class WordManager {
 	}
 	
 	public void start() {
+		wordCRUD.loadFile();
 		while(true) {
 			int menu = selectMenu();
 			if(menu==0) break;
